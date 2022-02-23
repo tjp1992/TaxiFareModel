e7b7263a26368cc4f89e814bad63387d06ea25bf
TJ Park <tjp1992@gmail.com>
2022-02-23 10:53:28 +0900

    code updated

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
+import mlflow
 
 class Trainer():
     def __init__(self, X, y):
@@ -53,6 +54,28 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         return compute_rmse(y_pred, y_test)
 
+    # ML-Flow
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 if __name__ == "__main__":
     # get data
--- ml_flow_local_test.py
+++ ml_flow_local_test.py
@@ -1,26 +1,13 @@
-# Server
-import mlflow
+# Testing Locally
 from mlflow.tracking import MlflowClient
-
-EXPERIMENT_NAME = "[KR] [Seoul] [TJ]"
-
-# Indicate mlflow to log to remote server
-mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
-
+EXPERIMENT_NAME = "test_experiment"
 client = MlflowClient()
-
 try:
     experiment_id = client.create_experiment(EXPERIMENT_NAME)
 except BaseException:
     experiment_id = client.get_experiment_by_name(EXPERIMENT_NAME).experiment_id
 
-yourname = '[KR] [Seoul] [TJ]'
-
-if yourname is None:
-    print("please define your name, il will be used as a parameter to log")
-
 for model in ["linear", "Randomforest"]:
     run = client.create_run(experiment_id)
     client.log_metric(run.info.run_id, "rmse", 4.5)
     client.log_param(run.info.run_id, "model", model)
-    client.log_param(run.info.run_id, "student_name", yourname)
